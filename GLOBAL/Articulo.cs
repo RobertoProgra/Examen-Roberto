8e8ad888f05a4ecb0485db0feb6efa90c85be529
RobertoProgra <robertoalvarado085@gmail.com>
2016-06-15 21:29:31 -0600

    first commit

---
+++ GLOBAL/Articulo.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Global
+{
+    class Articulo
+    {
+        private int codigo_articulo;
+        private string nombre_articulo;
+        private string nombre_proveedor;
+        private string tipo;
+        private int  posicion_estante;
+        private int fecha_vencimiento;
+        private double precio;
+        public double size;
+
+        private struct articulo
+        {
+            public int codigo_articulo;
+            public string nombre_articulo;
+            public string nombre_proveedor;
+            public string tipo;
+            public int posicion_estante;
+            public int fecha_vencimiento;
+            public double precio;
+            
+        };
+
+        private static articulo[] articulos;
+
+        //constructor
+        public Articulo()
+        {
+            codigo_articulo = 0;
+            nombre_articulo = "";
+            nombre_proveedor = "";
+            tipo = "";
+            posicion_estante = 0;
+            fecha_vencimiento = 0;
+            precio = 0;
+        }
+
+        //contructor con parámetros
+        public Articulo(int id, string nombre, string proveedor,string tipo,int  pos,int fecha, double precio)
+        {
+            codigo_articulo = id;
+            nombre_articulo = nombre;
+            nombre_proveedor = proveedor;
+            this.tipo = tipo;
+            posicion_estante = pos;
+            fecha_vencimiento = fecha;
+            this.precio = precio;
+        }
+
+        
+        //propiedades 
+        public void Setcodigo_articulo(int id)
+        {
+            codigo_articulo = id;
+        }
+
+        public int Getcodigo_articulo()
+        {
+            return codigo_articulo;
+        }
+
+        public void Setnombre_articulo(string nombre)
+        {
+            nombre_articulo = nombre;
+        }
+
+        public string Getnombre_articulo()
+        {
+            return nombre_articulo;
+        }
+
+        public void Setnombre_proveedor(string proveedor)
+        {
+            nombre_proveedor = proveedor;
+        }
+
+        public string Getnombre_proveedor()
+        {
+            return nombre_proveedor;
+        }
+
+        public void Settipo(string tipo)
+        {
+            this.tipo = tipo;
+        }
+
+        public string Gettipo()
+        {
+            return tipo;
+        }
+
+        public void Setposicion_estante(int pos)
+        {
+            posicion_estante = pos;
+        }
+
+        public int Getposicion_estante()
+        {
+            return posicion_estante;
+        }
+
+        public void Setfecha_vencimiento(int fecha)
+        {
+            fecha_vencimiento = fecha;
+        }
+
+        public int Getfecha_vencimiento()
+        {
+            return fecha_vencimiento;
+        }
+
+        public void Setprecio(double precio)
+        {
+            this.precio = precio;
+        }
+
+        public double Getprecio()
+        {
+            return precio;
+        }
+
+        public void SetSize(int Size)
+        {
+            size = Size;
+            articulos = new articulo[Size];
+        }
+
+        public void Initdata()
+        {
+            //inicializar
+            for (int i = 0; i < articulos.Length; i++)
+            {
+                articulos[i].codigo_articulo = 0;
+                articulos[i].nombre_articulo = "";
+                articulos[i].nombre_proveedor = "";
+                articulos[i].tipo = "";
+                articulos[i].posicion_estante = 0;
+                articulos[i].fecha_vencimiento = 0;
+                articulos[i].precio = 0;
+
+
+            }
+        }
+
+        public bool AddData(Global.Articulo articulo)
+        {
+            //agregar datos
+            int i;
+            for (i = 0; i < articulos.Length; i++)
+            {
+                if (articulos[i].codigo_articulo == 0)
+                {
+                    break;
+                }
+            }
+
+            if (i < articulos.Length)
+            {
+                articulos[i].codigo_articulo = articulo.codigo_articulo;
+                articulos[i].nombre_articulo = articulo.nombre_articulo;
+                articulos[i].nombre_proveedor = articulo.nombre_proveedor;
+                articulos[i].tipo = articulo.tipo;
+                articulos[i].posicion_estante = articulo.posicion_estante;
+                articulos[i].fecha_vencimiento = articulo.fecha_vencimiento;
+                articulos[i].precio = articulo.precio;
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool SeekData(int record)
+        {
+            //buscar datos
+            int i;
+            for (i = 0; i < articulos.Length; i++)
+            {
+                if (articulos[i].codigo_articulo == record)
+                {
+                    break;
+                }
+            }
+
+            if (i < articulos.Length)
+            {
+                codigo_articulo = articulos[i].codigo_articulo;
+                nombre_articulo = articulos[i].nombre_articulo;
+                nombre_proveedor = articulos[i].nombre_proveedor;
+                tipo = articulos[i].tipo;
+                posicion_estante = articulos[i].posicion_estante;
+                fecha_vencimiento = articulos[i].fecha_vencimiento;
+                precio = articulos[i].precio;
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool UpdateData(Global.Articulo articulo, int record)
+        {
+            //actualizar datos
+            int i;
+            for (i = 0; i < articulos.Length; i++)
+            {
+                if (articulos[i].codigo_articulo == record)
+                {
+                    break;
+                }
+            }
+
+            if (i < articulos.Length)
+            {
+                articulos[i].codigo_articulo = articulo.codigo_articulo;
+                articulos[i].nombre_articulo = articulo.nombre_articulo;
+                articulos[i].nombre_proveedor = articulo.nombre_proveedor;
+                articulos[i].tipo = articulo.tipo;
+                articulos[i].posicion_estante = articulo.posicion_estante;
+                articulos[i].fecha_vencimiento = articulo.fecha_vencimiento;
+                articulos[i].precio = articulo.precio;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool DeleteData(int record)
+        {
+            //borrar datos
+            int i;
+            for (i = 0; i < articulos.Length; i++)
+            {
+                if (articulos[i].codigo_articulo == record)
+                {
+                    break;
+                }
+            }
+
+            if (i < articulos.Length)
+            {
+                articulos[i].codigo_articulo = 0;
+                articulos[i].nombre_articulo = "";
+                articulos[i].nombre_proveedor = "";
+                articulos[i].tipo = "";
+                articulos[i].posicion_estante = 0;
+                articulos[i].fecha_vencimiento = 0;
+                articulos[i].precio = 0;
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void PrintData(int pos)
+        {
+            //imprimir todo el registro
+            codigo_articulo = articulos[pos].codigo_articulo;
+            nombre_articulo = articulos[pos].nombre_articulo;
+            nombre_proveedor = articulos[pos].nombre_proveedor;
+            tipo = articulos[pos].tipo;
+            posicion_estante = articulos[pos].posicion_estante;
+            fecha_vencimiento = articulos[pos].fecha_vencimiento;
+            precio = articulos[pos].precio;
+        }
+
+        // destructor
+        ~Articulo()
+        {
+
+        }
+    }
+}
---
+++ GLOBAL/Program.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+
+namespace Global
+{
+    class Program
+    {
+        static Articulo articulo = new Articulo();
+
+        static int option = 0;
+        static int size = 3;
+        [Flags]
+        enum BookPrice
+        {
+            Bajo = 500,
+            MedioBajo = 2500,
+            Medio = 5000,
+            MedioAlto = 7500,
+            Alto = 10000,
+        };
+
+        static void AddArticulo()
+        {
+            //agregar articulo
+            int r = 0, i= 0;
+            while (r != 1)
+            {
+                Console.Clear();
+                if (i < articulo.size)
+                {
+                    Console.WriteLine("-* Global Machines  *-*");
+                    Console.WriteLine("Ingreso de articulos.             ");
+                    Console.Write("Digite el ID:                  ");
+                    articulo.Setcodigo_articulo(int.Parse(Console.ReadLine()));
+                    Console.Write("Digite el nombre del libro:    ");
+                    articulo.Setnombre_articulo(Console.ReadLine());
+                
+                    if (articulo.AddData(articulo) == true)
+                        Console.WriteLine("Articulo agregado correctamente");
+                    else
+                        Console.WriteLine("Articulo no agregado");
+                    Console.WriteLine("Desea agregar otro Articulo 0-Sí 1-No");
+                    r = int.Parse(Console.ReadLine());
+                    i++;
+                }
+                else
+                {
+                    Console.WriteLine("Registro de Articulos lleno..");
+                    r = 1;
+                    Console.ReadKey();
+                }
+            }
+
+        }
+
+        static void ShowBooks()
+        {
+            //listar Articulos
+            int reg = 0, i;
+            Console.Clear();
+            Console.WriteLine("-* Global Machines  *-*");
+            Console.WriteLine("Listado de Articulos.             ");
+            for (i = 0; i < articulo.size; i++)
+            {
+                articulo.PrintData(i);
+                reg = i + 1;
+                Console.WriteLine("Registro: " + reg);
+                Console.WriteLine("ID:       " + articulo.Getcodigo_articulo());
+                Console.WriteLine("Nombre:   " + articulo.Getnombre_articulo());
+                Console.WriteLine("Nombre Proveedor:" + articulo.Getnombre_proveedor());
+                Console.WriteLine("Tipo" + articulo.Gettipo());
+                Console.WriteLine("Posicion en estante: " + articulo.Getposicion_estante());
+                Console.WriteLine("Fecha de Vencimiento:" + articulo.Getfecha_vencimiento());
+                Console.WriteLine("precio" + articulo.Getprecio());
+            }
+
+            Console.ReadKey();
+        }
+
+        static void SeekBooks()
+        {
+            //buscar articulo
+            int ID = 0, reg = 0, r = 0;
+            while (r != 1)
+            {
+                Console.Clear();
+                Console.WriteLine("-* Global Machines  *-*");
+                Console.WriteLine("Búsquedad de articulos.           ");
+                Console.WriteLine("Digite el ID del aticulo a buscar");
+                ID = int.Parse(Console.ReadLine());
+                if (articulo.SeekData(ID) == true)
+                {
+                    reg = ID;
+                    Console.WriteLine("Registro: " + reg);
+                    Console.WriteLine("ID:       " + articulo.GetBookID());
+                    Console.WriteLine("Nombre:   " + articulo.GetBookName());
+                    /*Console.WriteLine("Auto:     " + );
+                    Console.WriteLine("Precio:   " + );*/
+                }
+                else
+                    Console.WriteLine("No se encontro ese ID");
+                Console.WriteLine("Desea buscar otro libro 0-Sí 1-No");
+                r = int.Parse(Console.ReadLine());
+            }
+        }
+
+        static void PrintInvoice()
+        {
+            string currentPath = Path.Combine(Directory.GetParent(System.IO.Directory.GetCurrentDirectory()).Parent.FullName);
+            Console.WriteLine(currentPath + @"\Invoice.txt");
+
+            StreamWriter sw = new StreamWriter(currentPath + @"\Invoice.txt");
+            //Imprimiendo la factura
+            sw.WriteLine("===================================================================");
+            sw.WriteLine("*-*                  Librería El Buen Lector                    *-*");
+            sw.WriteLine("===================================================================");
+            sw.WriteLine(" Factura Proforma\t\t\t\t\t    N°0001");
+            sw.Write(" Cliente : ");
+            sw.WriteLine("Jorge Cordoba");
+            sw.Write(" Teléfono: ");
+            sw.WriteLine("88775599");
+            sw.WriteLine("-------------------------------------------------------------------");
+            sw.WriteLine(" Item\t\t\t\tCtd\tPrecio\t\tSubTotal");
+            sw.WriteLine("-------------------------------------------------------------------");
+            sw.Write(" El Señor de los anillos, las..");
+            sw.Write("\t" + "1");
+            sw.Write("\t¢" + "4999,99");
+            sw.WriteLine("\t¢" + "4999.99");
+            sw.WriteLine("-------------------------------------------------------------------");
+            sw.Write(" Total Orden:    \t\t\t\t\t");
+            sw.WriteLine("¢" + "4999.99");
+            sw.Write(" Total Descuento:    \t\t\t\t\t");
+            sw.WriteLine("¢" + "499,99");
+            sw.Write(" Impuesto Venta: \t\t\t\t\t");
+            sw.WriteLine("13" + "%");
+            sw.Write(" Impuesto Total: \t\t\t\t\t");
+            sw.WriteLine("¢" + "539.99");
+            sw.Write(" Precio Neto:    \t\t\t\t\t");
+            sw.WriteLine("¢" + "4999,99");
+            sw.WriteLine("-------------------------------------------------------------------");
+            sw.Write(" Monto Recibido:");
+            sw.WriteLine("¢" + "50000");
+            sw.Write(" Monto Cambio  :");
+            sw.WriteLine("¢" + "1");
+            sw.WriteLine("===================================================================");
+            sw.Write(" Gracias por comprar con nosotros...");
+            sw.Close();
+        }
+
+        static void Main()
+        {
+            //inicializar el objeto
+            articulo.SetSize(size);
+            articulo.Initdata();
+
+            PrintInvoice();
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("-* Librería el Buen Lector  *-*");
+                Console.WriteLine("1. Ingreso de libros.");
+                Console.WriteLine("2. Modificación de libros.");
+                Console.WriteLine("3. Eliminación de libros.");
+                Console.WriteLine("4. Búsquedad de libros.");
+                Console.WriteLine("5. Listado de libros.");
+                Console.WriteLine("6. Venta de libros.");
+                Console.WriteLine("7. Salir.");
+                Console.WriteLine("Selecciones una opción");
+                option = int.Parse(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        {                            
+                            AddArticulo();
+                            break;
+                        }
+                    case 2:
+                        {
+                            //Modificar la información
+                            break;
+                        }
+                    case 3:
+                        {
+                            //Eliminar información dejando campos en 0 o vacío
+                            break;
+                        }
+                    case 4:
+                        {
+                            SeekBooks();
+                            break;
+                        }
+                    case 5:
+                        {
+                            ShowBooks();
+                            break;
+                        }
+                    case 6:
+                        {
+                            //Venta similar a tarea1
+                            break;
+                        }
+                    case 7:
+                        //Salir
+                        break;
+                    default:
+                        {
+                            Console.WriteLine("Seleccione inválida..");
+                            Console.ReadKey();
+                            Console.Clear();
+                            break;
+                        }
+                }
+            } while (option != 7);
+        }
+    }
+}
